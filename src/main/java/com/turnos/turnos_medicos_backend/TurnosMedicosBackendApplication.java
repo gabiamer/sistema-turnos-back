156fce75563a6029bee0ceea44d87e822c18ddb4
GabMer <ana.gabrielucb.edu.bo>
2026-04-02 22:18:00 -0400

    feat: configuracion de entorno sb

---
+++ src/main/java/com/turnos/turnos_medicos_backend/TurnosMedicosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.turnos.turnos_medicos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnosMedicosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnosMedicosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turnos/turnos_medicos_backend/TurnosMedicosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turnos.turnos_medicos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnosMedicosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
