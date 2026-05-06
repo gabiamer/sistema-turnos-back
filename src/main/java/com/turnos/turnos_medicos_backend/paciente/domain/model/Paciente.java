b84d21f5574405960a5064098149b53e9d979376
GabMer <ana.gabrielucb.edu.bo>
2026-05-05 22:39:55 -0400

    feat: completar modelos Paciente, Turno y EstadoTurno - Sprint 1

--- src/main/java/com/turnos/turnos_medicos_backend/paciente/domain/model/Paciente.java
+++ src/main/java/com/turnos/turnos_medicos_backend/paciente/domain/model/Paciente.java
@@ -1,7 +1,8 @@
-package com.turnos.turnos_medicos_backend.domain.model;
+package com.turnos.turnos_medicos_backend.paciente.domain.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -16,11 +17,20 @@ public class Paciente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true, length = 20)
+    private String ci;
+
     @Column(nullable = false, length = 100)
     private String nombre;
 
     @Column(nullable = false, length = 100)
     private String apellido;
+
+    @Column(name = "fecha_nacimiento")
+    private LocalDate fechaNacimiento;
+
+    @Column(length = 20)
+    private String telefono;
 
     @Column(nullable = false, unique = true, length = 150)
     private String email;
